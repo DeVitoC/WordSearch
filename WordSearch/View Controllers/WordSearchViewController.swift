493b388d39a04fa27f94f60b3b53026e63f3e457
Christopher DeVito <christopher.devito@protonmail.com>
2020-05-25 23:46:16 -0400

    Removed all storyboard letter buttons and outlets and ibactions for those buttons.

--- WordSearch/View Controllers/WordSearchViewController.swift
+++ WordSearch/View Controllers/WordSearchViewController.swift
@@ -14,7 +14,7 @@ class WordSearchViewController: UIViewController {
     let wordController = WordController()
     let gameBoardController = GameBoardController()
     private var word: Word?
-    private lazy var letterButtons: [UIButton] = [letter1Button, letter2Button, letter3Button, letter4Button, letter5Button, letter6Button]
+    private lazy var letterButtons: [UIButton] = []
     private var wordInProgress: String = ""
     private var wordMapLabels: [[UILabel]] = []
     private var gameBoard: GameBoard? {
@@ -31,12 +31,6 @@ class WordSearchViewController: UIViewController {
     var buttonsStackView = UIStackView(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
     
     // MARK: - IBOutlets
-    @IBOutlet weak var letter1Button: UIButton!
-    @IBOutlet weak var letter2Button: UIButton!
-    @IBOutlet weak var letter3Button: UIButton!
-    @IBOutlet weak var letter4Button: UIButton!
-    @IBOutlet weak var letter5Button: UIButton!
-    @IBOutlet weak var letter6Button: UIButton!
     @IBOutlet weak var checkWordButton: UIButton!
     @IBOutlet var buttons: UIStackView!
 
@@ -55,14 +49,6 @@ class WordSearchViewController: UIViewController {
         generateButtons()
         let mainWordChars: [Character] = Array(word.mainWord)
         print("\(mainWordChars)")
-        letter1Button.setTitle("\(mainWordChars[0].uppercased())", for: .normal)
-        letter2Button.setTitle("\(mainWordChars[1].uppercased())", for: .normal)
-        letter3Button.setTitle("\(mainWordChars[2].uppercased())", for: .normal)
-        letter4Button.setTitle("\(mainWordChars[3].uppercased())", for: .normal)
-        letter5Button.setTitle("\(mainWordChars[4].uppercased())", for: .normal)
-        letter6Button.setTitle("\(mainWordChars[5].uppercased())", for: .normal)
-
-
     }
 
     // MARK: - Set Up Methods
@@ -150,72 +136,6 @@ class WordSearchViewController: UIViewController {
     }
 
     // MARK: - IBActions
-    @IBAction func letter1Tapped(_ sender: Any) {
-        if letter1Button.isSelected {
-            letter1Button.isSelected = false
-        } else {
-            letter1Button.isSelected = true
-            guard let letter = letter1Button.titleLabel?.text else { return }
-            wordInProgress.append(letter)
-            print(wordInProgress)
-        }
-    }
-
-    @IBAction func letter2Tapped(_ sender: Any) {
-        if letter2Button.isSelected {
-            letter2Button.isSelected = false
-        } else {
-            letter2Button.isSelected = true
-            guard let letter = letter2Button.titleLabel?.text else { return }
-            wordInProgress.append(letter)
-            print(wordInProgress)
-        }
-    }
-
-    @IBAction func letter3Tapped(_ sender: Any) {
-        if letter3Button.isSelected {
-            letter3Button.isSelected = false
-        } else {
-            letter3Button.isSelected = true
-            guard let letter = letter3Button.titleLabel?.text else { return }
-            wordInProgress.append(letter)
-            print(wordInProgress)
-        }
-    }
-
-    @IBAction func letter4Tapped(_ sender: Any) {
-        if letter4Button.isSelected {
-            letter4Button.isSelected = false
-        } else {
-            letter4Button.isSelected = true
-            guard let letter = letter4Button.titleLabel?.text else { return }
-            wordInProgress.append(letter)
-            print(wordInProgress)
-        }
-    }
-
-    @IBAction func letter5Tapped(_ sender: Any) {
-        if letter5Button.isSelected {
-            letter5Button.isSelected = false
-        } else {
-            letter5Button.isSelected = true
-            guard let letter = letter5Button.titleLabel?.text else { return }
-            wordInProgress.append(letter)
-            print(wordInProgress)
-        }
-    }
-
-    @IBAction func letter6Tapped(_ sender: Any) {
-        if letter6Button.isSelected {
-            letter6Button.isSelected = false
-        } else {
-            letter6Button.isSelected = true
-            guard let letter = letter6Button.titleLabel?.text else { return }
-            wordInProgress.append(letter)
-            print(wordInProgress)
-        }
-    }
-
     @IBAction func checkWord(_ sender: Any) {
         guard let word = word else { return }
         if word.searchWords.contains(wordInProgress.lowercased()) {
