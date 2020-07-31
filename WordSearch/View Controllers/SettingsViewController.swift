d6d9e1f5e2fb57f44c40257c6d1b3ede513b6f4d
Christopher DeVito <christopher.devito@protonmail.com>
2020-07-31 04:30:10 -0400

    Added json files for additional languages: Danish, Dutch, French, Italian, German, Spanish

--- WordSearch/View Controllers/SettingsViewController.swift
+++ WordSearch/View Controllers/SettingsViewController.swift
@@ -172,17 +172,6 @@ class SettingsViewController: UIViewController {
             defaults.setOptionalBool(value: false, forKey: key)
         }
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
 extension SettingsViewController: UIPickerViewDataSource, UIPickerViewDelegate {
